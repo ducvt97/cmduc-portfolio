f93166e20b7c483273fc845838308667f1cc6003
ducvt97 <ducvt97@gmail.com>
2024-04-27 17:55:26 +0700

    Update Contact section in HomePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
 import { Epilogue } from "next/font/google";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import "@/assets/styles/css/globals.css";
 import "@/assets/styles/scss/index.scss"
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
+config.autoAddCss = false
 
 const inter = Epilogue({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import { faFacebook, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="">
@@ -73,13 +78,53 @@ export default function Home() {
       </section>
       <section className="section bg-dark gap-10">
         <div className="card card-white flex-1">
-          <h3>Development</h3>
-          <div className="">
-            Hire to outsource your digital marketing efforts, instead of
-            handling in-house can provide your business.
+          <h4>Get in touch</h4>
+          <div className="input-field">
+            <input type="text" id="email" required className="input" />
+            <label htmlFor="email">Email</label>
           </div>
+          <div className="input-field">
+            <input type="text" id="subject" required className="input" />
+            <label htmlFor="email">Subject</label>
+          </div>
+          <div className="input-field">
+            <input type="text" id="message" required className="input" />
+            <label htmlFor="email">Message</label>
+          </div>
+          <button className="btn btn-primary w-max self-end">Send</button>
         </div>
-        <div className="h-96 flex-1 bg-slate-900"></div>
+        <div className="flex flex-1 flex-col gap-3 sm:pl-16">
+          <h3 className="mb-5">Contact Me</h3>
+          <div className="flex">
+            <FontAwesomeIcon icon={faEnvelope} className="text-xl mr-3" />
+            ducvt97@gmail.com
+          </div>
+          <div className="flex">
+            <FontAwesomeIcon icon={faPhone} className="text-xl mr-3" />
+            (+84) 868 459 305
+          </div>
+          <Link
+            href="https://www.linkedin.com/in/minhducchu97/"
+            className="flex underline"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="text-xl mr-3" />
+            linkedin.com/minhducchu97
+          </Link>
+          <Link
+            href="https://www.facebook.com/minhduc.chu.97/"
+            className="flex underline"
+          >
+            <FontAwesomeIcon icon={faFacebook} className="text-xl mr-3" />
+            facebook.com/minhduc.chu.97
+          </Link>
+          <Link
+            href="https://github.com/ducvt97"
+            className="flex underline"
+          >
+            <FontAwesomeIcon icon={faGithub} className="text-xl mr-3" />
+            github.com/ducvt97
+          </Link>
+        </div>
       </section>
     </main>
   );
